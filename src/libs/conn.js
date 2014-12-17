150fba71e9c04f5cdb6828acf59c20a40c3bd943
Thiago Kroger <thiagokroger@gmail.com>
2014-12-16 22:53:09 -0200

    go

--- src/libs/conn.js
+++ src/libs/conn.js
@@ -145,7 +145,9 @@ module.exports = {
             fields += (fields=="") ? obj.indexes[key] : ","+obj.indexes[key];
             where += (where=="") ? obj.indexes[key]+"='"+obj.fields[obj.indexes[key]]+"'" : ","+obj.indexes[key]+"='"+obj.fields[obj.indexes[key]]+"'";
         }
-
+        console.log("-----------------------");
+        console.log(obj);
+        console.log("-----------------------");
         return "SELECT " + fields + " FROM " + obj.table + " WHERE " + where;
 
     },
@@ -156,24 +158,21 @@ module.exports = {
         var getUpdateSql = this.getUpdateSql;
         var getInsertSql = this.getInsertSql;
         var getExistsSql = this.getExistsSql;
-        connection.connect();
+        var freeQuery = this.freeQuery;
 
         var parent = (obj.parent) ? obj.parent : obj;
 
         var sql = "";
 
-        connection.query(getExistsSql(parent), function(errExists, parentExists) {
-            console.log(errExists);
-            if (parentExists.length > 0){
+        freeQuery(getExistsSql(parent)).then(function(parentExists) {
+            if (parentExists.rows.length > 0){
                 sql = getUpdateSql(parent);
             }else{
                 sql = getInsertSql(parent);
             }
-            console.log(sql);
-
             //connection.query(sql, function(err, result) {
 
-                if (common.isEmpty(errExists)){
+                if (common.isEmpty(parentExists.error)){
                     if (common.isEmpty(obj.children)){
                         d.resolve({
                             err:null,
@@ -184,17 +183,16 @@ module.exports = {
                         var ct=0;
                         for (var key in obj.children){
                             children = obj.children[key];
-                            console.log(children);
-                            connection.query(getExistsSql(children), function(errExists, childrenExists) {
-                                if (childrenExists.length > 0){
+                            freeQuery(getExistsSql(children)).then(function(childrenExists) {
+                                if (childrenExists.rows.length > 0){
                                     sql = getUpdateSql(children);
                                 }else{
                                     sql = getInsertSql(children);
                                 }
                                 console.log(sql);
-                                connection.quer(sql, function(err, childrenResult) {
+                                //connection.quer(sql, function(err, childrenResult) {
                                     ct++;
-                                });
+                                //});
                             });
 
                             if (ct==obj.children.length){
