954263d5d85e2862cc38d2defac985c8f39494a5
Thiago Kroger <thiagokroger@gmail.com>
2015-03-03 23:31:26 -0300

    go

--- src/configs/cors_config.js
+++ src/configs/cors_config.js
@@ -1,15 +1,17 @@
 module.exports = {
-  allowedOrigins: [
-    'http://127.0.0.1:9000',
-    'http://192.168.25.84:9000',
-    'http://201.87.228.140:9000',
-    'http://177.72.244.6',
-    'http://beet.cc',
-    'http://beet.com.br',
-    'http://192.168.16.58:9000',
-    'http://127.0.0.1:3000',
-    'http://localhost:3000',
-    'http://localhost'
-  ]
+    allowedOrigins: [
+        'http://127.0.0.1:9000',
+        'http://192.168.25.84:9000',
+        'http://201.87.228.140:9000',
+        'http://177.72.244.6',
+        'http://beet.cc',
+        'http://beet.com.br',
+        'http://192.168.16.58:9000',
+        'http://127.0.0.1:3000',
+        'http://localhost:3000',
+        'http://192.168.25.17:3000',
+        'http://localhost'
+
+    ]
 };
 
