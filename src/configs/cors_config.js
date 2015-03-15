b24e23061c2d98d660040c28fa497ca03f77e457
gpferreira <gustavoferreira@MacBook-Pro-de-Gustavo.local>
2015-03-14 23:15:42 -0300

    go

--- src/configs/cors_config.js
+++ src/configs/cors_config.js
@@ -6,6 +6,7 @@ module.exports = {
         'http://177.72.244.6',
         'http://beet.cc',
         'http://beet.com.br',
+        'http://app.beet.com.br',
         'http://192.168.16.58:9000',
         'http://127.0.0.1:3000',
         'http://localhost:3000',
