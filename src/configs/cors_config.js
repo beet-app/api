f23ba712219103a758cc33fb82cabaa81120d631
gpferreira <gustavoferreira@MacBook-Pro-de-Gustavo.local>
2015-04-26 20:22:07 -0300

    go

--- src/configs/cors_config.js
+++ src/configs/cors_config.js
@@ -12,6 +12,7 @@ module.exports = {
         'http://127.0.0.1:3000',
         'http://localhost:3000',
         'http://192.168.25.17:3000',
+        'http://192.168.25.7:3000',
         'http://localhost'
 
     ]
